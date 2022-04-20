94c83fedabf4e4e2b8ab8c45b1c08b168738f5ca
Dennis FOX <dfox@foxartbox.com>
2022-04-20 22:12:09 +0300

    Home page video

--- templates/p3week_2k18/index.php
+++ templates/p3week_2k18/index.php
@@ -168,21 +168,34 @@ unset(
 		<video class="home_video_new__item" autoplay loop muted>
 			<source src="/images/2022/home.mp4" type="video/mp4" />
 		</video>
+
+		<div class="home_video_new__content">
+			<a href="/" class="logo"><img class="js-svg" src="/images/icons/logo_2022.svg" alt=""></a>
+		</div>
+
+		<div class="home_video_new__bottom">
+			<div class="home_video_new__info">
+				<div class="home_video_new__date">12-15 СЕНТЯБРЯ 2022 г.</div>
+
+				<div class="home_video_new__place">Цифровое Деловое Пространство,<br />г. Москва, ул. Покровка, 47</div>
+			</div>
+		</div>
 	</div>
+<?php }
+else { ?>
+	<?php if ($this->countModules('header')) { ?>
+		<div class="header">
+			<div class="wrap">
+				<jdoc:include type="modules" name="header" style="none" />
+			</div>
+		</div>
+	<?php } ?>
 <?php } ?>
 
 <?php if($this->countModules('search')) { ?>
 	<div class="search__wrap">
 		<div class="wrap">
 			<jdoc:include type="modules" name="search" style="none" />
-		</div>
-	</div>
-<?php } ?>
-
-<?php if ($this->countModules('header')) { ?>
-	<div class="header">
-		<div class="wrap">
-			<jdoc:include type="modules" name="header" style="none" />
 		</div>
 	</div>
 <?php } ?>
