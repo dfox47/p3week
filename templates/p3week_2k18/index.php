2f7f54480048e5d8993f3ac0ffef515a210545f3
Dennis FOX <dfox@foxartbox.com>
2022-04-21 19:47:19 +0300

    Home video to template parts.

--- templates/p3week_2k18/index.php
+++ templates/p3week_2k18/index.php
@@ -13,12 +13,8 @@ $jinput             = JFactory::getApplication()->input;
 $page_id            = $jinput->get('id'); ?>
 
 <?php $this->setGenerator(null);
-
 unset($doc->_scripts[$this->baseurl . '/media/jui/js/bootstrap.min.js']); // Remove joomla core bootstrap
-
-unset(
-	$this->_scripts[$this->baseurl.'/media/system/js/mootools-core.js']
-); ?>
+unset($this->_scripts[$this->baseurl.'/media/system/js/mootools-core.js']); ?>
 
 <!DOCTYPE html>
 
@@ -55,8 +51,6 @@ unset(
 	<jdoc:include type="head" />
 </head>
 
-
-
 <body class="lang_<?php echo $this->language; ?> no_js">
 
 <?php // menu slider
@@ -65,59 +59,8 @@ include_once 'template-parts/menu_slider.php'; ?>
 <?php // menu top
 include_once 'template-parts/menu_top.php'; ?>
 
-<?php if (!isset($_SERVER['REQUEST_URI']) || ltrim($_SERVER['REQUEST_URI'],'/') === '') { ?>
-	<div class="home_video_new">
-		<video class="home_video_new__item" autoplay loop muted>
-			<source src="/images/2022/home.mp4" type="video/mp4" />
-		</video>
-
-		<div class="home_video_new__content">
-			<a href="/" class="logo"><img class="js-svg" src="/images/icons/logo_2022.svg" alt=""></a>
-		</div>
-
-		<div class="home_video_new__bottom">
-			<div class="home_video_new__info">
-				<div class="home_video_new__date">12-15 СЕНТЯБРЯ 2022 г.</div>
-
-				<div class="home_video_new__place">Цифровое Деловое Пространство,<br />г. Москва, ул. Покровка, 47</div>
-
-				<button class="btn btn_submit js-popup-show" data-popup="registration">Регистрация</button>
-			</div>
-		</div>
-	</div>
-<?php }
-else if ( $_SERVER['REQUEST_URI'] === '/en/') { ?>
-	<div class="home_video_new">
-		<video class="home_video_new__item" autoplay loop muted>
-			<source src="/images/2022/home.mp4" type="video/mp4" />
-		</video>
-
-		<div class="home_video_new__content">
-			<a href="/" class="logo"><img class="js-svg" src="/images/icons/logo_2022_en.svg" alt=""></a>
-		</div>
-
-		<div class="home_video_new__bottom">
-			<div class="home_video_new__info">
-				<div class="home_video_new__date">12-15 SEPTEMBER 2022</div>
-
-				<div class="home_video_new__place">SAP Digital Business Space,<br />Moscow, Pokrovka 47</div>
-
-				<button class="btn btn_submit js-popup-show" data-popup="registration">Registration</button>
-			</div>
-		</div>
-	</div>
-<?php }
-else { ?>
-	<?php if ($this->countModules('header')) { ?>
-		<div class="header">
-			<div class="wrap">
-				<jdoc:include type="modules" name="header" style="none" />
-			</div>
-		</div>
-	<?php } ?>
-<?php } ?>
-
-
+<?php // menu top
+include_once 'template-parts/home_video.php'; ?>
 
 <?php if ($this->countModules('p3transport__link')) { ?>
 	<jdoc:include type="modules" name="p3transport__link" style="none" />
