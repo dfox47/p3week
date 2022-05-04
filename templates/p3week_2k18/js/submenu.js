db8672b87cba6e5db596b6e725ce415b92239b1a
Dennis FOX <dfox@foxartbox.com>
2022-05-04 23:30:18 +0300

    Submenu js bugs fixed.

--- templates/p3week_2k18/js/submenu.js
+++ templates/p3week_2k18/js/submenu.js
@@ -1,11 +1,19 @@
 
-let $submenu        = document.querySelector('.submenu').offsetTop
-let $html           = document.querySelector('html')
+let submenu = () => {
+	let $submenu        = document.querySelector('.submenu')
 
-let submenuSticky = () => {
-	$html.classList.toggle('submenu_fixed', window.pageYOffset > $submenu)
+	if (!$submenu) return
+
+	let submenuOffset   = $submenu.offsetTop
+	let $html           = document.querySelector('html')
+
+	let submenuSticky = () => {
+		$html.classList.toggle('submenu_fixed', window.pageYOffset > submenuOffset)
+	}
+
+	window.onscroll = () => {
+		submenuSticky()
+	}
 }
 
-window.onscroll = () => {
-	submenuSticky()
-}
+submenu()
