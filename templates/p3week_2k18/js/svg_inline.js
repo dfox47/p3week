4697f67018693130e677f983407811b6fa7f0642
Dennis FOX <dfox@foxartbox.com>
2022-05-05 00:00:11 +0300

    Svg inline optimisation.

--- templates/p3week_2k18/js/svg_inline.js
+++ templates/p3week_2k18/js/svg_inline.js
@@ -3,21 +3,22 @@ var $ = jQuery.noConflict();
 $(window).bind("load", function() {
 	// replace SVG with inline SVG code to change color with CSS
 	$('img.svg, img.js-svg').each(function(){
-		var $img = $(this);
-		var imgID = $img.attr('id');
-		var imgClass = $img.attr('class');
-		var imgURL = $img.attr('src');
+		let $img        = $(this);
+		let imgID       = $img.attr('id');
+		let imgClass    = $img.attr('class');
+		let imgURL      = $img.attr('src');
 
 		$.get(imgURL, function(data) {
 			// Get the SVG tag, ignore the rest
-			var $svg = $(data).find('svg');
+			let $svg = $(data).find('svg');
 
 			// Add replaced image's ID to the new SVG
-			if(typeof imgID !== 'undefined') {
+			if (typeof imgID !== 'undefined') {
 				$svg = $svg.attr('id', imgID);
 			}
+
 			// Add replaced image's classes to the new SVG
-			if(typeof imgClass !== 'undefined') {
+			if (typeof imgClass !== 'undefined') {
 				$svg = $svg.attr('class', imgClass+' replaced-svg');
 			}
 
