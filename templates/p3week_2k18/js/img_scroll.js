1f4d314389117fba73189819b30eddc240eb2a30
Dennis FOX <dfox@foxartbox.com>
2022-05-14 23:25:49 +0300

    Img scroll pure JS version

--- templates/p3week_2k18/js/img_scroll.js
+++ templates/p3week_2k18/js/img_scroll.js
@@ -24,3 +24,25 @@ function img_scroll() {
 		}
 	})
 }
+
+
+
+// pure JS version
+let changeImgSrc = () => {
+	document.querySelectorAll('.js-img-scroll').forEach((e) => {
+		if (window.pageYOffset + window.innerHeight > e.offsetTop) {
+			e.classList.remove('js-img-scroll')
+			e.src = e.dataset.src
+		}
+	})
+}
+
+changeImgSrc()
+
+window.addEventListener('resize', function() {
+	changeImgSrc()
+})
+
+window.addEventListener('scroll', function() {
+	changeImgSrc()
+})
