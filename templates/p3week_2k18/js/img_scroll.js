f939477c3e9009a3dcf6351e7602ddbbec2a357b
Dennis FOX <dfox@foxartbox.com>
2022-05-14 22:38:48 +0300

    Img scroll refactoring

--- templates/p3week_2k18/js/img_scroll.js
+++ templates/p3week_2k18/js/img_scroll.js
@@ -11,15 +11,16 @@ $(window).on('load', function () {
 
 function img_scroll() {
 	$('img.js-img_scroll').each(function () {
-		let top_of_element      = $(this).offset().top
-		let bottom_of_element   = $(this).offset().top + $(this).outerHeight()
+		let $this               = $(this)
+		let top_of_element      = $this.offset().top
+		let bottom_of_element   = $this.offset().top + $this.outerHeight()
 		let bottom_of_screen    = $(window).scrollTop() + $(window).innerHeight()
 		let top_of_screen       = $(window).scrollTop()
 
 		if ( (bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element) ) {
-			let img_src = $(this).attr('data-img-src')
+			let img_src = $this.attr('data-img-src')
 
-			$(this).removeClass('js-img_scroll').attr( 'src', img_src )
+			$this.removeClass('js-img_scroll').attr( 'src', img_src )
 		}
 	})
 }
