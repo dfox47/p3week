a78684c750732b188bafe16a6281e4f215874701
fox@teamidea.ru <Zn2g47hZqeDvl>
2022-06-24 20:32:00 +0300

    Img scroll refactoring.

--- templates/p3week_2k18/js/img_scroll.js
+++ templates/p3week_2k18/js/img_scroll.js
@@ -2,25 +2,25 @@
 var $ = jQuery.noConflict()
 
 $(window).on('load', function () {
-	img_scroll()
-
-	$(window).on('resize scroll', function() {
-		img_scroll()
-	})
+	imgScroll()
 })
 
-function img_scroll() {
-	$('img.js-img_scroll').each(function () {
-		let $this               = $(this)
-		let top_of_element      = $this.offset().top
-		let bottom_of_element   = $this.offset().top + $this.outerHeight()
-		let bottom_of_screen    = $(window).scrollTop() + $(window).innerHeight()
-		let top_of_screen       = $(window).scrollTop()
+window.addEventListener('resize', () => {
+	imgScroll()
+})
 
-		if ( (bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element) ) {
+window.addEventListener('scroll', () => {
+	imgScroll()
+})
+
+function imgScroll() {
+	$('img.js-imgScroll').each(function () {
+		let $this = $(this)
+
+		if ( ($(window).scrollTop() + $(window).innerHeight() > $this.offset().top) && ($(window).scrollTop() < $this.offset().top + $this.outerHeight()) ) {
 			let img_src = $this.attr('data-img-src')
 
-			$this.removeClass('js-img_scroll').attr( 'src', img_src )
+			$this.removeClass('js-imgScroll').attr( 'src', img_src )
 		}
 	})
 }
