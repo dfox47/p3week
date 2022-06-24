e6cda256c9f79a8c9adb143504eeb94d3a8ff0d2
fox@teamidea.ru <Zn2g47hZqeDvl>
2022-06-24 21:06:28 +0300

    Viemo thumb JS refactoring.

--- templates/p3week_2k18/js/vimeo_thumbnail.js
+++ templates/p3week_2k18/js/vimeo_thumbnail.js
@@ -1,34 +1,36 @@
-var $ = jQuery.noConflict();
+
+var $ = jQuery.noConflict()
 
 $(window).bind("load", function() {
 	// get thumbnails from vimeo.com
-	drawVimeoImages();
+	drawVimeoImages()
 
 	function getVimeoThumbnail(id) {
 		$.ajax({
-			type:			'GET',
-			url:			'http://vimeo.com/api/v2/video/' + id + '.json',
-			jsonp:			'callback',
-			dataType:		'jsonp',
-			success:		function(data) {
-				var thumbnail_src = data[0].thumbnail_large;
-				$('[data-vimeo-id='+id+']').attr('src', thumbnail_src);
+			type:           'GET',
+			url:            'http://vimeo.com/api/v2/video/' + id + '.json',
+			jsonp:          'callback',
+			dataType:       'jsonp',
+			success:        function(data) {
+				let thumbSrc = data[0].thumbnail_large
+
+				$('[data-vimeo-id='+id+']').attr('src', thumbSrc)
 			}
-		});
+		})
 	}
 
 	function drawVimeoImages() {
-		var vimeoImgDataAttr			= 'data-vimeo-id',
-			vimeoThumbnails				= $('[' + vimeoImgDataAttr + ']'),
-			vimeoThumbnailsLength		= vimeoThumbnails.length;
+		let vimeoImgDataAttr        = 'data-vimeo-id'
+		let vimeoThumbnails         = $('[' + vimeoImgDataAttr + ']')
+		let vimeoThumbnailsLength   = vimeoThumbnails.length
 
 		if(vimeoThumbnailsLength) {
 			for(var i=0, l = vimeoThumbnailsLength; i < l; i++) {
-				var vimeoImg		= $(vimeoThumbnails).get(i),
-					vimeoImgId		= $(vimeoImg).attr(vimeoImgDataAttr);
+				let vimeoImg        = $(vimeoThumbnails).get(i)
+				let vimeoImgId      = $(vimeoImg).attr(vimeoImgDataAttr)
 
-				getVimeoThumbnail(vimeoImgId);
+				getVimeoThumbnail(vimeoImgId)
 			}
 		}
-	};
-});
+	}
+})
